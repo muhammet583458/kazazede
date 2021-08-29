d9cb62448c3c676b33e933c31f70f9c5ddc38f92
Muhammet <tayyipakbulut@gmail.com>
2021-08-29 15:07:17 +0300

    first

---
+++ src/main/java/akbulut/kazazede/KazazedeApplication.java
@@ -0,0 +1,13 @@
+package akbulut.kazazede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazazedeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KazazedeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akbulut/kazazede/KazazedeApplicationTests.java
@@ -0,0 +1,13 @@
+package akbulut.kazazede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazazedeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
